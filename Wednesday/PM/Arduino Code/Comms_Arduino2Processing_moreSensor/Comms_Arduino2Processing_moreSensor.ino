b9da4b79a83129f9faaa0be6ddc59d446c19ec2c
Becca Rose <hello@beccarose.co.uk>
2021-10-06 14:06:20 +0100

    Update Comms_Arduino2Processing_moreSensor.ino

--- Wednesday/PM/Arduino Code/Comms_Arduino2Processing_moreSensor/Comms_Arduino2Processing_moreSensor.ino
+++ Wednesday/PM/Arduino Code/Comms_Arduino2Processing_moreSensor/Comms_Arduino2Processing_moreSensor.ino
@@ -1,86 +1,28 @@
-import processing.serial.*;
 
-// read data from arduino to processing  and store it in 3 values
-
-float sensorX = 0;
-float sensorY = 0;
-float sensorZ = 0;
-
-//---------------------
+// 1026mapa
+// ashley james brown
+// more than 1 sensor
 
 void setup() {
-  size(800, 600);
-  background(0);
-  setupSerial();
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
 }
 
-//---------------------
-void draw() {
-  background(0);
-  fill(255, 27, 0);
-  ellipse(sensorX, sensorY, 100, 100);
- // println(sensorX);
-}
+void loop() {
+  // put your main code here, to run repeatedly:
+  float s1 = analogRead(A0);
 
+  delay(100);
+  float s2 = analogRead(A1);
+  delay (100);
 
-//------------------------------------------------------
+  Serial.print("xValue:");
+  Serial.print(s1);
+  Serial.print(",");
 
-
-Serial myPort;  // Create object from Serial class
-int val;      // Data received from the serial port
-
-
-void setupSerial() {
-  printArray(Serial.list());
-  try {
-    String portName = Serial.list()[9]; //change the number to suit your board
-    myPort = new Serial(this, portName, 9600);  //115200
-    println("connected to -->  " +portName);
-    myPort.bufferUntil(','); //enables to split the data via a comma which is set in teh arduino code
-  } 
-  catch (Exception e) {
-    println("no device connection found");
-  }
-}
-
-
-
-
-void serialEvent(Serial myPort) {
-
-  String inString = myPort.readStringUntil(',');
-  // split the string into multiple strings
-  // where there is a delimter":"
-
-  //println(inString); //data line coming in
-
-  // chop up the line by : into an array to split the data
-  String items[] = split(inString, ':');
-
-  printArray(items);
-
-  // if there was more than one string after the split (there will be the text and the data)
-  if (items.length > 1) {
-    String label = trim(items[0]);
-    // remove the ',' off the end
-    String val = split(items[1], ',')[0];
-    println(val);
-
-    // check what the label was and update the appropriate variable
-    if (label.equals("xValue")) {
-      println("looks like sensor X   "+val);
-      sensorX = float(val);
-    } 
-    // check what the label was and update the appropriate variable
-    if (label.equals("yValue")) {
-      println("looks like sensor Y   "+val);
-      sensorY= float(val);
-    } 
-
-    // check what the label was and update the appropriate variable
-    if (label.equals("zValue")) {
-      println("looks like sensor Z   "+val);
-      sensorZ = float(val);
-    }
-  }
+  Serial.print("yValue:");
+  Serial.print(s2);
+  Serial.println(",");
 }
